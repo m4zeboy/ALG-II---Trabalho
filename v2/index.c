358dc0595f8a6137c5afbdbe438cb8baa1e0241d
m4zeboy <moises775azevedo@gmail.com>
2022-08-27 22:37:18 -0300

    Big commit of saturday night

--- v2/index.c
+++ v2/index.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+    Fiz algumas alterações nas nomenclaturas e retirei aquela documentação extensa.
+    Procurei deixar as funções mais específicas do que as funções da versão anterior, agora o fluxo de atendimento está melhor dividido em várias funções menores.
+    Essa divisão se fez necessária pois me deparei com muitas funções recebendo muitos parâmetros, então tentei deixar a função menor, e receber o mínimo de parâmetros.
+
+*/
 
 typedef struct {
   int id, idade;
@@ -23,32 +29,29 @@ typedef struct
     void menu_pinricpal(char *op) {
         printf("\nAtendimento ao Cidadão\n");
         printf("----------------------\n");
-        printf("1 - Cidadão\n");
-        printf("2 - Gerar Senha\n");
-        printf("3 - Atendimento\n");
-        printf("S - Sair\n");
+        printf("1 - Cidadão\n2 - Gerar Senha\n3 - Atendimento\n4 - Relatórios\nS - Sair\n");
         scanf(" %c", op);
     }
 
     void menu_cidadao(char *op_cid) {
         printf("\nCidadão\n");
         printf("----------------------\n");
-        printf("1 - Cadastrar Cidadao\n");
-        printf("2 - Pesquisar por código\n");
-        printf("3 - Alterar informação\n");
-        printf("4 - Remover\n");
-        printf("0 - Voltar\n");
+        printf("1 - Cadastrar Cidadao\n2 - Pesquisar por código\n3 - Alterar informação\n4 - Remover\n0 - Voltar\n");
         scanf(" %c", op_cid);
     }
 
     void menu_atualiza_cidadao(char *op_at_cid) {
         printf("\nAtualiza Informação\n");
         printf("----------------------\n");
-        printf("1 - Atualizar Nome\n");
-        printf("2 - Atualizar Idade\n");
-        printf("3 - Atualizar UF\n");
-        printf("0 - Voltar\n");
+        printf("1 - Atualizar Nome\n2 - Atualizar Idade\n3 - Atualizar UF\n0 - Voltar\n");
         scanf(" %c", op_at_cid);
+    }
+
+    void menu_relatorios(char *op_rel) {
+        printf("\nRelatórios\n");
+        printf("----------------------\n");
+        printf("\n1 - Cidadão Cadastrados\n2 - Senhas Não Atendidas\n3 - Atendimentos Realzados\n0 - Voltar\n");
+        scanf(" %c", op_rel);
     }
 /* menus */
 
@@ -173,7 +176,7 @@ typedef struct
         }
     }
 
-    void troca_cidadao(cidadao *a, cidadao *b) {
+    void trocaCidadao(cidadao *a, cidadao *b) {
         cidadao aux;
         aux = *a;
         *a = *b;
@@ -185,7 +188,7 @@ typedef struct
         for(i=0;i<tam_cid;i++) {
             for(j=i+1;j<tam_cid;j++) {
                 if(cidadaos[i].id > cidadaos[j].id) {
-                    troca_cidadao(&cidadaos[i], &cidadaos[j]);
+                    trocaCidadao(&cidadaos[i], &cidadaos[j]);
                 }
             }
         }    
@@ -199,7 +202,7 @@ typedef struct
         indice = pesquisaCidadaoPorID(cidadaos, tam_cid, id);
         if(indice != -1) {
             for (i = indice; i - 1 < *tam_cid; i++) {
-                troca_cidadao(&cidadaos[i], &cidadaos[i + 1]);
+                trocaCidadao(&cidadaos[i], &cidadaos[i + 1]);
             }
             *tam_cid = *tam_cid - 1;
             printf("Cidadão Removido com sucesso.\n");
@@ -213,99 +216,144 @@ typedef struct
 /* cidadão */
 
 /* senha */
-
-void gerarSenha(cidadao cidadaos[], int *tam_cid, solicitacao_de_atendimento fila[], int *tam_fila, solicitacao_de_atendimento fila_preferencial[], int *tam_fila_preferencial, int *senhas_na_fila) {
-    solicitacao_de_atendimento s_at;
-    cidadao c;
-    int indice,id;
-    printf("ID: ");
-    scanf("%d", &id);
-    indice = pesquisaCidadaoPorID(cidadaos, tam_cid, id);
-    if(indice != -1) {
-        c = cidadaos[indice];
-        printf("Serviço solicitado: ");
-        scanf(" %c", &s_at.servico_solicitado);
-        if(c.idade >= 65) {
-            /* colocar na fila preferencial */
-            sprintf(s_at.prioridade, "PREFERENCIAL");
-            s_at.senha = *tam_fila_preferencial;
-            fila_preferencial[*tam_fila_preferencial] = s_at;;
-            (*tam_fila_preferencial)++;
-            (*senhas_na_fila)++;
+    void lerSolicitacaoDeAtendimento(solicitacao_de_atendimento *s_at, cidadao cidadaos[], int *tam_cid) {
+        int id, indice;
+        printf("ID: ");
+        scanf("%d", &id);
+        indice = pesquisaCidadaoPorID(cidadaos, tam_cid, id);
+        if(indice != -1) {
+            s_at->id_cid = cidadaos[indice].id;
+            printf("Serviço solicitado: ");
+            scanf(" %c", &s_at->servico_solicitado);
         } else {
-            /* colocar na fila não preferencial */
-            sprintf(s_at.prioridade, "NÃO PREFERENCIAL");
-            s_at.senha = *tam_fila;
-            fila[*tam_fila] = s_at;
-            (*tam_fila)++;
-            (*senhas_na_fila)++;
+            printf("Cidadão não encontrado ou removido.\n");
         }
-
-    } else {
-        printf("Cidadão não encontrado ou removido.\n");
     }
 
-}
-
-void escreverSenha(FILE *stream, solicitacao_de_atendimento senha) {
-    fprintf(stream, "ID_CID: %d;PRIORIDADE: %s;SENHA: %d;SERVIÇO SOLICITADO: %c\n", senha.id_cid, senha.prioridade, senha.senha, senha.servico_solicitado);
-}
-void escreverFila(FILE *stream, solicitacao_de_atendimento fila[], int tam_fila) {
-    int i;
-    fprintf(stream, "%d;\n", tam_fila);
-    for(i=0; i<tam_fila;i++) {
-        escreverSenha(stream, fila[i]);
+    void definirPreferenciaDaSenha(solicitacao_de_atendimento *s_at, cidadao cidadaos[], int *tam_cid) {
+        int indice;
+        indice = pesquisaCidadaoPorID(cidadaos, tam_cid, s_at->id_cid);
+        if(indice != -1) {
+            if(cidadaos[indice].idade >= 65) {
+                sprintf(s_at->prioridade, "Preferencial");
+            } else {
+                sprintf(s_at->prioridade, "Não Preferencial");
+            }
+        } else {
+            printf("Cidadão não encontrado ou removido.\n");
+        }
     }
-}
+
+    void gerarSenha(solicitacao_de_atendimento *s_at, int tam_fila) {
+        s_at->senha=tam_fila+1;
+    }
+
+    void adicionarNaFila(solicitacao_de_atendimento s_at, solicitacao_de_atendimento fila[], int *tam_fila) {
+        fila[*tam_fila] = s_at;
+        (*tam_fila)++;
+    }
+
+    void escreverSenha(FILE *stream, solicitacao_de_atendimento senha) {
+        fprintf(stream, "ID_CID: %d;PRIORIDADE: %s;SENHA: %d;SERVIÇO SOLICITADO: %c\n", senha.id_cid, senha.prioridade, senha.senha, senha.servico_solicitado);
+    }
+    
+    void escreverFila(FILE *stream, solicitacao_de_atendimento fila[], int tam_fila, solicitacao_de_atendimento fila_preferencial[], int tam_fila_preferencial) {
+        int i;
+        fprintf(stream, "%d;\n", tam_fila_preferencial);
+        fprintf(stream, "%d;\n", tam_fila);
+        for(i=0; i<tam_fila_preferencial;i++) {
+            escreverSenha(stream, fila_preferencial[i]);
+        }
+         for(i=0; i<tam_fila;i++) {
+            escreverSenha(stream, fila[i]);
+        }
+    }
 
 /* senha */
 
 /* atendimento ao cidadao */
-atendimento iniciarAtendimento() {
-    atendimento at;
-    printf("Servidor: ");
-    scanf(" %[^\n]", at.servidor);
-    printf("Mesa: ");
-    scanf("%d",  &at.mesa);
-    return at;
-}
-
-atendimento definirProximaSenha(solicitacao_de_atendimento fila[], int tam_fila, solicitacao_de_atendimento fila_preferencial[], int tam_fila_preferencial, atendimento at) {
-    if(tam_fila_preferencial > 0) {
-        /* atender o primeiro cidadao da fila preferencial*/
-        at.id_cid = fila_preferencial[0].id_cid;
-        at.senha = fila_preferencial[0].senha;
-        strcpy(at.prioridade, fila_preferencial[0].prioridade);
-        at.servico_solicitado = fila_preferencial[0].servico_solicitado;
-    } else {
-        /* atender o primeiro cidadao da fila */
-        at.id_cid = fila[0].id_cid;
-        at.senha = fila[0].senha;
-        strcpy(at.prioridade, fila[0].prioridade);
-        at.servico_solicitado = fila[0].servico_solicitado;
+    atendimento iniciarAtendimento() {
+        atendimento at;
+        printf("Servidor: ");
+        scanf(" %[^\n]", at.servidor);
+        printf("Mesa: ");
+        scanf("%d",  &at.mesa);
+        return at;
     }
-    return at;
-}
 
-void chamarProximaSenha(atendimento at) {
+    void definirProximaSenha(solicitacao_de_atendimento fila[], int tam_fila, solicitacao_de_atendimento fila_preferencial[], int tam_fila_preferencial, atendimento *at) {
+        if(tam_fila_preferencial > 0) {
+            /* atender o primeiro cidadao da fila preferencial*/
+            at->id_cid = fila_preferencial[0].id_cid;
+            at->senha = fila_preferencial[0].senha;
+            strcpy(at->prioridade, fila_preferencial[0].prioridade);
+            at->servico_solicitado = fila_preferencial[0].servico_solicitado;
+        } else {
+            /* atender o primeiro cidadao da fila */
+            at->id_cid = fila[0].id_cid;
+            at->senha = fila[0].senha;
+            strcpy(at->prioridade, fila[0].prioridade);
+            at->servico_solicitado = fila[0].servico_solicitado;
+        }
+    }
+
+    void chamarProximaSenha(atendimento at) {
     printf("\nPróxima senha a ser atendida: %d\n", at.senha);
-    printf("\nTipo de sertviço: %c\tMesa: %d\n", at.servico_solicitado, at.mesa);
+    printf("Tipo de sertviço: %c\tMesa: %d\n", at.servico_solicitado, at.mesa);
 
 }
+
+    void deslocaSenhas(solicitacao_de_atendimento fila[], int *tam_fila) {
+        int i;
+        for(i=0;i<*tam_fila;i++) {
+            fila[i] = fila[i+1];
+        }
+        (*tam_fila)--;
+    }
+
+    void fechaAtendimento(atendimento at, atendimento atendimentos_realizados[], int *tam_at_rlzd) {
+        atendimentos_realizados[*tam_at_rlzd] = at;
+        (*tam_at_rlzd)++;
+    }
+
+    void escreverAtendimento(FILE *stream, atendimento at) {
+        fprintf(stream, "ID_CID: %d;MESA: %d;SENHA: %d;SERVIDOR: %s;PRIORIDADE: %s;SERVICO_SOLICITADO: %c\n", at.id_cid, at.mesa, at.senha, at.servidor, at.prioridade, at.servico_solicitado);
+    }
+
+    void trocaAtendimento(atendimento *a, atendimento *b) {
+        atendimento aux;
+        aux = *a;
+        *a = *b;
+        *b = aux;
+    }
+
+    void ordenarAtendimento(atendimento atendimentos[], int tam_at_rlzd) {
+        int i, j;
+        for(i=0;i<tam_at_rlzd;i++) {
+            for(j=i+1;j<tam_at_rlzd;j++) {
+                if(atendimentos[i].senha > atendimentos[j].senha) {
+                    trocaAtendimento(&atendimentos[i], &atendimentos[j]);
+
+                }
+            }
+        }
+    }
+
 /* atendimento ao cidadao */
 int main(void) {
     cidadao cidadaos[500];
-    int tam_cid, indice_cid, id, tam_fila, tam_fila_preferencial;
-    char op, op_cid, op_at_cid;
+    int tam_cid, indice_cid, id, tam_fila, tam_fila_preferencial, tam_at_rlzd, i;
+    char op, op_cid, op_at_cid, op_rel;
     int senhas_na_fila;
-    solicitacao_de_atendimento fila[100], fila_preferencial[100];
-    atendimento at;
+    solicitacao_de_atendimento fila[100], fila_preferencial[100], s_at;
+    atendimento at, atendimentos_realizados[500];
     FILE *arq;
 
     tam_cid = 0;
     tam_fila=0;
     tam_fila_preferencial=0;
     senhas_na_fila = 0;
+    tam_at_rlzd=0;
 
     lerCidadaosDoArquivo(cidadaos, &tam_cid);
     
@@ -362,16 +410,118 @@ int main(void) {
 
         if(op ==  '2') {
             if((tam_fila + tam_fila_preferencial) <= 100) {
-                gerarSenha(cidadaos, &tam_cid, fila, &tam_fila, fila_preferencial,&tam_fila_preferencial, &senhas_na_fila);            
+                lerSolicitacaoDeAtendimento(&s_at, cidadaos, &tam_cid);
+                definirPreferenciaDaSenha(&s_at, cidadaos, &tam_cid);
+                if(strcmp(s_at.prioridade,"Preferencial") == 0)  {
+                    gerarSenha(&s_at, tam_fila_preferencial);
+                    adicionarNaFila(s_at,fila_preferencial, &tam_fila_preferencial); 
+                    senhas_na_fila++;
+                } else {
+                    gerarSenha(&s_at, tam_fila);
+                    adicionarNaFila(s_at,fila, &tam_fila); 
+                    senhas_na_fila++;
+                }
             } else
                 printf("Hoje já atingiu 100 solicitações de atendimento.");
         }
 
         if(op == '3') {
             /* realizar atendimento */
-            at = iniciarAtendimento();
+            if(senhas_na_fila > 0) {
+                at = iniciarAtendimento();
+                definirProximaSenha(fila,tam_fila, fila_preferencial,tam_fila_preferencial,&at);
+                chamarProximaSenha(at);
+                if(tam_fila_preferencial>0) {
+                    deslocaSenhas(fila_preferencial, &tam_fila_preferencial);
+                } else {
+                    deslocaSenhas(fila, &tam_fila);
+                }
+                fechaAtendimento(at, atendimentos_realizados,&tam_at_rlzd);
+                senhas_na_fila--;
+            } else {
+                printf("\nAinda não há pessoas na fila para serem atendidas.\n");
+            }
+
         }
 
+        if(op == '4') {
+            /* relatorios */
+            menu_relatorios(&op_rel);
+            switch (op_rel)
+            {
+            case '1':
+                /* cidadaos cadastrados */
+                arq = fopen("cidadaos.csv", "w");
+                if(arq == NULL) {
+                    printf("Não foi possível abrir o arquivo cidadaos.csv");
+                } else {
+                    escreveVetorCidadaos(arq, cidadaos, tam_cid);
+                    fclose(arq);
+                }
+                break;
+            case '2':
+                /* senhas nao atendidas*/
+                arq = fopen("proximas.csv", "w");
+                if(arq == NULL) {
+                    printf("Não foi possível abrir o arquivo proximas.csv");
+                } else {
+                    escreverFila(arq, fila, tam_fila, fila_preferencial, tam_fila_preferencial);
+                    fclose(arq);
+                }
+                break;
+            case '3':
+                /* atendimentos realizados */
+                ordenarAtendimento(atendimentos_realizados, tam_at_rlzd);
+                /*DOCUMENTOS*/
+                arq = fopen("documentos.csv", "w");
+                if(arq == NULL) {
+                    printf("Não foi possível abrir o arquivo documentos.csv");
+                } else {
+                    /*procurar apenas os atendimentos realizados pelo serviço de DOCUMENTOS*/
+                    for(i=0;i<tam_at_rlzd;i++) {
+                        if(atendimentos_realizados[i].servico_solicitado == 'D') {
+                            escreverAtendimento(arq, atendimentos_realizados[i]);
+                        }
+                    }     
+                    fclose(arq);
+                }
+                /*DOCUMENTOS*/
+                
+                /*TRANSPORTE*/
+                arq = fopen("transporte.csv", "w");
+                if(arq == NULL) {
+                    printf("Não foi possível abrir o arquivo transporte.csv");
+                } else {
+                    /*procurar apenas os atendimentos realizados pelo serviço de TRANSPORTE*/
+                    for(i=0;i<tam_at_rlzd;i++) {
+                        if(atendimentos_realizados[i].servico_solicitado == 'T') {
+                            escreverAtendimento(arq, atendimentos_realizados[i]);
+                        }
+                    }     
+                    fclose(arq);
+                }
+                /*TRANSPORTE*/
+
+                /*TRANSPORTE*/
+                arq = fopen("moradia.csv", "w");
+                if(arq == NULL) {
+                    printf("Não foi possível abrir o arquivo transporte.csv");
+                } else {
+                    /*procurar apenas os atendimentos realizados pelo serviço de TRANSPORTE*/
+                    for(i=0;i<tam_at_rlzd;i++) {
+                        if(atendimentos_realizados[i].servico_solicitado == 'M') {
+                            escreverAtendimento(arq, atendimentos_realizados[i]);
+                        }
+                    }     
+                    fclose(arq);
+                }
+                /*TRANSPORTE*/
+                
+            
+            default:
+                break;
+            }
+        }
         /* saída */
         if(op=='s' || op == 'S') {
             if(senhas_na_fila > 0) {
