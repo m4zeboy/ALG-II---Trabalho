0e41f15d1a76470f88010d4d08529a7ee01f0461
m4zeboy <moises775azevedo@gmail.com>
2022-11-01 13:48:41 -0300

    v3 - cidadao>persistencia,op:cadastrar, pesquisar

---
+++ v3/index.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct cidadao {
+  int codigo, idade;
+  char nome[30], estado[3];
+  struct cidadao *prox;
+} Cidadao;
+
+Cidadao *buscaCidadao(Cidadao *lista, int codigo) {
+  Cidadao *achado = lista;
+  while(achado && achado->codigo != codigo) {
+    achado=achado->prox;
+  }
+  return achado;
+} 
+
+void insereCidadaoEmOrdem(Cidadao **lista, Cidadao dados) {
+  Cidadao *p, *q, *nova;
+  p = NULL; q = *lista;
+  while(q && q->codigo < dados.codigo) {
+    p = q; 
+    q = q->prox;
+  }
+  nova = (Cidadao *) malloc(sizeof(Cidadao));
+  if(nova) {
+    nova->codigo = dados.codigo;
+    strcpy(nova->nome, dados.nome);
+    nova->idade = dados.idade;
+    strcpy(nova->estado, dados.estado);
+    nova->prox = q;
+    if(p == NULL) {
+      *lista = nova;
+    } else {
+      p->prox = nova;
+    }
+  }
+}
+
+void _mostraCidadaos(Cidadao *lista) {
+  if(lista) {
+    printf("Codigo: %d Nome: %s Idade: %d UF: %s\n", 
+    lista->codigo,
+    lista->nome,
+    lista->idade,
+    lista->estado
+    );
+    _mostraCidadaos(lista->prox);
+  }
+}
+
+void carregaCidadaos(Cidadao **lista) {
+  FILE *arq;
+  Cidadao dados;
+  arq = fopen("cidadaos.data", "r");
+  if(arq) {
+    while(1) {
+      fscanf(
+        arq,
+        "%d;%[^;];%d;%[^\n]\n",
+        &(dados.codigo),
+        dados.nome,
+        &(dados.idade),
+        dados.estado
+      );
+      if(feof(arq)){
+        break;
+      }
+      insereCidadaoEmOrdem(lista, dados);
+    }
+    fclose(arq);
+  } else {
+    printf("Não foi possível abrir o arquivo cidadaos.data.\n");
+    *lista = NULL;
+  } 
+}
+
+void salvarCidadaos(Cidadao *lista) {
+  FILE *arq;
+  Cidadao *p;
+  p = lista;
+  arq = fopen("cidadaos.data", "w");
+  if(arq) {
+    while(p) {
+      fprintf(
+        arq,
+        "%d;%s;%d;%s\n",
+        p->codigo,
+        p->nome,
+        p->idade,
+        p->estado
+      );
+      p = p->prox;
+    }
+    fclose(arq);
+  } else {
+    printf("Não foi possível abrir o arquivo cidadaos.data.\n");
+  } 
+}
+
+int main(void) {
+  Cidadao *lista, dados;
+  char op, op_sub;
+  lista=NULL;
+  carregaCidadaos(&lista);
+  do {
+    printf("1 CIDADAO\n2 GERAR SENHA\n3 ATENDIMENTO AO CIDADAO\n4 RELATORIOS\nS SAIR\n");
+    scanf(" %c", &op);
+    if(op == '1') {
+      do {
+        printf("1 CADASTRAR\n2 PESQUISAR POR CODIGO\n3 ATUALIZAR CADASTRO\n4 EXCLUIR CADASTRO\n0 VOLTAR\n");
+        scanf(" %c", &op_sub);
+
+        /* 1. cadastrar */
+        if(op_sub == '1') {
+          printf("Codigo: ");
+          scanf("%d", &(dados.codigo));
+          if(buscaCidadao(lista, dados.codigo) != NULL) {
+            printf("Cidadao ja cadastrado com esse codigo.\n");
+          } else {
+            /* cadastrar cidadão novo*/
+            printf("Nome: ");
+            scanf(" %[^\n]", dados.nome);
+
+            printf("Idade: ");
+            scanf("%d", &(dados.idade));
+
+            printf("Estado: ");
+            scanf(" %[^\n]", dados.estado);
+
+            insereCidadaoEmOrdem(&lista, dados);
+          }
+        }
+
+      } while(op_sub != '0');
+    }
+
+  } while(op != 's' && op != 'S');
+  _mostraCidadaos(lista);
+  salvarCidadaos(lista);
+  return 0;
+}
