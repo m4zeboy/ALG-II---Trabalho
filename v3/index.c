68eb27eae031c9d824623700025bdf67ed69ecbb
m4zeboy <moises775azevedo@gmail.com>
2022-11-03 12:02:39 -0300

    v3 - atualiza>nome

--- v3/index.c
+++ v3/index.c
@@ -107,7 +107,7 @@ void salvarCidadaos(Cidadao *lista) {
 
 int main(void) {
   Cidadao *lista, dados, *temp;
-  char op,  op_sub;
+  char op,  op_sub, op_at;
   lista=NULL;
   carregaCidadaos(&lista);
   do {
@@ -118,42 +118,72 @@ int main(void) {
         printf("1 CADASTRAR\n2 PESQUISAR POR CODIGO\n3 ATUALIZAR CADASTRO\n4 EXCLUIR CADASTRO\n0 VOLTAR\n");
         scanf(" %c", &op_sub);
 
-        /* 1. cadastrar */
-        if(op_sub == '1') {
-          printf("Codigo: ");
-          scanf("%d", &(dados.codigo));
-          if(buscaCidadao(lista, dados.codigo) != NULL) {
-            printf("Cidadao ja cadastrado com esse codigo.\n");
-          } else {
-            /* cadastrar cidadão novo*/
-            printf("Nome: ");
-            scanf(" %[^\n]", dados.nome);
+        switch (op_sub){
+          case '1':
+          /* 1. cadastrar */
+            printf("Codigo: ");
+            scanf("%d", &(dados.codigo));
+            if(buscaCidadao(lista, dados.codigo) != NULL) {
+              printf("Cidadao ja cadastrado com esse codigo.\n");
+            } else {
+              /* cadastrar cidadão novo*/
+              printf("Nome: ");
+              scanf(" %[^\n]", dados.nome);
 
-            printf("Idade: ");
-            scanf("%d", &(dados.idade));
+              printf("Idade: ");
+              scanf("%d", &(dados.idade));
 
-            printf("Estado: ");
-            scanf(" %[^\n]", dados.estado);
+              printf("Estado: ");
+              scanf(" %[^\n]", dados.estado);
 
-            insereCidadaoEmOrdem(&lista, dados);
-          }
-        }
+              insereCidadaoEmOrdem(&lista, dados);
+            }
+            break;
+          case '2':
+          /* 2. pesquisar por codigo */
+            printf("Codigo: ");
+            scanf("%d", &(dados.codigo));
+            temp = buscaCidadao(lista, dados.codigo);
+            if(temp != NULL) {
+              printf("Codigo: %d Nome: %s Idade: %d UF: %s\n", 
+              temp->codigo,
+              temp->nome,
+              temp->idade,
+              temp->estado
+            );
+            } else {
+              printf("Cidadao nao encontrado.\n");
+            }
+            break;
+          case '3':
+          /* 3. atualizar cadasatro */
+            do {
+              printf("1 NOME\n2 IDADE\n3 UF\n0 VOLTAR\n");
+              scanf(" %c", &op_at);
+              switch (op_at) {
+                case '1':
+                  /* atualizar nome */
+                  printf("Informe o codigo do cidadao: ");
+                  scanf("%d", &(dados.codigo));
+                  temp = buscaCidadao(lista, dados.codigo);
+                  if(temp != NULL) {
+                    printf("Informe o novo nome: ");
+                    scanf(" %[^\n]", dados.nome);
+                    strcpy(temp->nome, dados.nome);
+                    printf("Nome atualizado com sucesso.\n");
+                  } else {
+                    printf("Cidadao nao encontrado.\n");
+                  }
+                  break;
 
-        /* 2. pesquisar por codigo */
-        if(op_sub == '2') {
-          printf("Codigo: ");
-          scanf("%d", &(dados.codigo));
-          temp = buscaCidadao(lista, dados.codigo);
-          if(temp != NULL) {
-            printf("Codigo: %d Nome: %s Idade: %d UF: %s\n", 
-            temp->codigo,
-            temp->nome,
-            temp->idade,
-            temp->estado
-          );
-          } else {
-            printf("Cidadao não encontrado.\n");
-          }
+                
+                default:
+                  break;
+              }
+            } while(op_at != '0');
+            break;
+          default:
+            break;
         }
       } while(op_sub != '0');
     }
