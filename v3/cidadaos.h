bddc2689739f229b3c5c6032ebafea89453dfea4
m4zeboy <moises775azevedo@gmail.com>
2022-11-04 20:52:05 -0300

    v3 - atualizar cadastro e excluir

---
+++ v3/cidadaos.h
@@ -0,0 +1,124 @@
+typedef struct cidadao {
+  int codigo, idade;
+  char nome[30], estado[3];
+  struct cidadao *prox;
+} Cidadao;
+
+Cidadao *buscaCidadao(Cidadao *lista, int codigo) {
+  Cidadao *achado = lista;
+  while(achado && achado->codigo != codigo) {
+    achado=achado->prox;
+  }
+  return achado;
+} 
+
+void insereCidadaoEmOrdem(Cidadao **lista, Cidadao dados) {
+  Cidadao *p, *q, *nova;
+  p = NULL; q = *lista;
+  while(q && q->codigo < dados.codigo) {
+    p = q; 
+    q = q->prox;
+  }
+  nova = (Cidadao *) malloc(sizeof(Cidadao));
+  if(nova) {
+    nova->codigo = dados.codigo;
+    strcpy(nova->nome, dados.nome);
+    nova->idade = dados.idade;
+    strcpy(nova->estado, dados.estado);
+    nova->prox = q;
+    if(p == NULL) {
+      *lista = nova;
+    } else {
+      p->prox = nova;
+    }
+  }
+}
+
+void _mostraCidadaos(Cidadao *lista) {
+  if(lista) {
+    printf("Codigo: %d Nome: %s Idade: %d UF: %s\n", 
+    lista->codigo,
+    lista->nome,
+    lista->idade,
+    lista->estado
+    );
+    _mostraCidadaos(lista->prox);
+  }
+}
+
+void carregaCidadaos(Cidadao **lista) {
+  FILE *arq;
+  Cidadao dados;
+  int status;
+  arq = fopen("cidadaos.data", "r");
+  if(arq) {
+    status = fscanf(
+        arq,
+        "%d;%[^;];%d;%[^\n]\n",
+        &(dados.codigo),
+        dados.nome,
+        &(dados.idade),
+        dados.estado
+      );
+    while(status != -1) {
+      insereCidadaoEmOrdem(lista, dados);  
+      status = fscanf(
+        arq,
+        "%d;%[^;];%d;%[^\n]\n",
+        &(dados.codigo),
+        dados.nome,
+        &(dados.idade),
+        dados.estado
+      );
+    }
+    
+    fclose(arq);
+  } else {
+    printf("Não foi possível abrir o arquivo cidadaos.data.\n");
+  } 
+}
+
+void salvarCidadaos(Cidadao *lista) {
+  FILE *arq;
+  Cidadao *p;
+  p = lista;
+  arq = fopen("cidadaos.data", "w");
+  if(arq) {
+    while(p) {
+      fprintf(
+        arq,
+        "%d;%s;%d;%s\n",
+        p->codigo,
+        p->nome,
+        p->idade,
+        p->estado
+      );
+      p = p->prox;
+    }
+    fclose(arq);
+  } else {
+    printf("Não foi possível abrir o arquivo cidadaos.data.\n");
+  } 
+}
+
+
+void excluiCadastro(Cidadao **lista, int codigo) {
+  Cidadao *p, *q;
+  p = NULL; q = *lista;
+  while(q && q->codigo != codigo) {
+    p = q;
+    q = q->prox;
+  }
+
+  if (q != NULL) {
+    if(p == NULL) {
+      *lista = q->prox;
+    } else {
+      p->prox = q->prox;
+    }
+    free(q);
+    printf("Cadastro excluido com sucesso.\n");
+  } else {
+    printf("Cidadao não encontrado.\n");
+  }
+}
--- v3/index.c
+++ v3/index.c
@@ -1,109 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct cidadao {
-  int codigo, idade;
-  char nome[30], estado[3];
-  struct cidadao *prox;
-} Cidadao;
-
-Cidadao *buscaCidadao(Cidadao *lista, int codigo) {
-  Cidadao *achado = lista;
-  while(achado && achado->codigo != codigo) {
-    achado=achado->prox;
-  }
-  return achado;
-} 
-
-void insereCidadaoEmOrdem(Cidadao **lista, Cidadao dados) {
-  Cidadao *p, *q, *nova;
-  p = NULL; q = *lista;
-  while(q && q->codigo < dados.codigo) {
-    p = q; 
-    q = q->prox;
-  }
-  nova = (Cidadao *) malloc(sizeof(Cidadao));
-  if(nova) {
-    nova->codigo = dados.codigo;
-    strcpy(nova->nome, dados.nome);
-    nova->idade = dados.idade;
-    strcpy(nova->estado, dados.estado);
-    nova->prox = q;
-    if(p == NULL) {
-      *lista = nova;
-    } else {
-      p->prox = nova;
-    }
-  }
-}
-
-void _mostraCidadaos(Cidadao *lista) {
-  if(lista) {
-    printf("Codigo: %d Nome: %s Idade: %d UF: %s\n", 
-    lista->codigo,
-    lista->nome,
-    lista->idade,
-    lista->estado
-    );
-    _mostraCidadaos(lista->prox);
-  }
-}
-
-void carregaCidadaos(Cidadao **lista) {
-  FILE *arq;
-  Cidadao dados;
-  int status;
-  arq = fopen("cidadaos.data", "r");
-  if(arq) {
-    status = fscanf(
-        arq,
-        "%d;%[^;];%d;%[^\n]\n",
-        &(dados.codigo),
-        dados.nome,
-        &(dados.idade),
-        dados.estado
-      );
-    while(status != -1) {
-      insereCidadaoEmOrdem(lista, dados);  
-      status = fscanf(
-        arq,
-        "%d;%[^;];%d;%[^\n]\n",
-        &(dados.codigo),
-        dados.nome,
-        &(dados.idade),
-        dados.estado
-      );
-    }
-    
-    fclose(arq);
-  } else {
-    printf("Não foi possível abrir o arquivo cidadaos.data.\n");
-  } 
-}
-
-void salvarCidadaos(Cidadao *lista) {
-  FILE *arq;
-  Cidadao *p;
-  p = lista;
-  arq = fopen("cidadaos.data", "w");
-  if(arq) {
-    while(p) {
-      fprintf(
-        arq,
-        "%d;%s;%d;%s\n",
-        p->codigo,
-        p->nome,
-        p->idade,
-        p->estado
-      );
-      p = p->prox;
-    }
-    fclose(arq);
-  } else {
-    printf("Não foi possível abrir o arquivo cidadaos.data.\n");
-  } 
-}
+#include "cidadaos.h"
 
 int main(void) {
   Cidadao *lista, dados, *temp;
@@ -175,12 +73,45 @@ int main(void) {
                     printf("Cidadao nao encontrado.\n");
                   }
                   break;
-
-                
+                case '2': 
+                  /* atualizar idade */
+                  printf("Informe o codigo do cidadao: ");
+                  scanf("%d", &(dados.codigo));
+                  temp = buscaCidadao(lista, dados.codigo);
+                  if(temp != NULL) {
+                    printf("Informe a nova idade: ");
+                    scanf("%d", &(dados.idade));
+                    temp->idade = dados.idade;
+                    printf("Idadae atualizada com sucesso.\n");
+                  } else {
+                    printf("Cidadao nao encontrado.\n");
+                  }
+                break;   
+                case '3':
+                  /* atualizar estado */
+                  printf("Informe o codigo do cidadao: ");
+                  scanf("%d", &(dados.codigo));
+                  temp = buscaCidadao(lista, dados.codigo);
+                  if(temp != NULL) {
+                    printf("Informe o novo estado: ");
+                    scanf(" %[^\n]", dados.estado);
+                    strcpy(temp->estado, dados.estado);
+                    printf("Estado atualizado com sucesso.\n");
+                  } else {
+                    printf("Cidadao nao encontrado.\n");
+                  }
+                break;             
                 default:
+                  printf("Opcao Invalida\n");
                   break;
               }
             } while(op_at != '0');
+            break;
+          case '4':
+          /* excluir cadastro */
+            printf("Informe o codigo do cidadao: ");
+            scanf("%d", &(dados.codigo));
+            excluiCadastro(&lista, dados.codigo);
             break;
           default:
             break;
